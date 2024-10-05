b45a7d7eea7914b6e464c6b3dfc96bddb16d7f22
LOOP  ∞ <175203647+sarveshsinghthakur@users.noreply.github.com>
2024-10-05 10:32:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 # from flask_sqlalchemy import SQLAlchemymport SQLAlchemy
@@ -78,4 +79,5 @@ def about():
 
 
 if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', dubug=True)
